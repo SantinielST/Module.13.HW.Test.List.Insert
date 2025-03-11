6a0f6209668d1e34d83993e356ae9e313a20a493
Name <email@example.ru>
2025-03-11 15:06:44 +0300

    Add project files.

---
+++ Module.13.HW.Test.List.Insert/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+
+namespace Module._13.HW.Test.List.Insert;
+
+internal class Program
+{
+    // Объявим простой список с именами
+    private static List<string> UsersNames = new List<string>()
+    {
+        "Фёдор",
+        "Сергей",
+        "Алесандр"
+    };
+
+    static void Main(string[] args)
+    {
+        var stopWatch = Stopwatch.StartNew(); // Запускаем таймер
+
+        UsersNames.Insert(2, "Павел"); // Добавляем имя по индексу в середину списка
+
+        Console.WriteLine($"Вставка в  словарь: {stopWatch.Elapsed.TotalMilliseconds}  мс"); // Выводим результат
+    }
+}
